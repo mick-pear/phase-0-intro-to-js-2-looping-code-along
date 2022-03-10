1040ebe202a028a3c64064bb65022a82cc0642b2
Matthew <matthew_mcaleer@hotmail.com>
2022-03-10 18:37:54 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let returnValue = [];
+
+function writeCards(person, giftType) {
+  for (let i = 0; i < person.length; i++) {
+    returnValue.push(`Thank you, ${person[i]}, for the wonderful ${giftType} gift!`)
+  }
+
+  return returnValue;
+}
+
+function countDown(counts) {
+  while (counts >= 0) {
+    console.log(counts--);
+  }
+}
